c3e82d2ad039813ae026c405a1d331cf3f1e8a1a
effort <497748354@qq.com>
2018-03-15 23:16:49 +0800

    修改代码架构

--- src/com/effort/encryption/AES256ForBC.java
+++ src/com/effort/encryption/AES256ForBC.java
@@ -1,90 +1,222 @@
 package com.effort.encryption;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.util.encoders.Base64;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.Security;
+import javax.print.DocFlavor;
+import java.io.*;
+import java.security.*;
 
 public class AES256ForBC {
-
-    private static byte[] encrypt(String password, String key) {
+    private static final String ALGORITHM = "AES";
+    /**
+     * 获取初始化之后的Cipher对象
+     *
+     * @param mode
+     *          加密/解密
+     * @param key
+     *             key
+     * @return
+     *          初始化之后的Cipher对象
+     */
+    private Cipher getCipher(int mode,String key){
+        Security.addProvider(new BouncyCastleProvider());
         try {
-            Security.addProvider(new BouncyCastleProvider());
-            //"AES"：请求的密钥算法的标准名称
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            SecureRandom securerandom = new SecureRandom(tohash256Deal(key));
-
-            kgen.init(256, securerandom);
-            SecretKey secretKey = kgen.generateKey();
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+            SecureRandom secureRandom = new SecureRandom(tohash256Deal(key));
+            keyGenerator.init(256,secureRandom);
+            SecretKey secretKey = keyGenerator.generateKey();
             byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
-
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            byte[] byteContent = password.getBytes("utf-8");
-            byte[] cryptograph = cipher.doFinal(byteContent);
-//            byte[] test = Base64.encode(cryptograph);
-//            System.out.println(AES256ForBC.parseByte2HexStr(test));
-//            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-//            byte[] content = cipher.doFinal(Base64.decode(test));
-//            System.out.println(new String(content));
-            return Base64.encode(cryptograph);
-        } catch (Exception e) {
+            SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat,ALGORITHM);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding","BC");
+            cipher.init(mode,secretKeySpec);
+            return cipher;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 加密
+     *
+     * @param content
+     *          需要加密的内容
+     * @param key
+     *          key
+     * @return
+     *          信息加密之后的数据
+     */
+    private byte[] encrypt(String content,String key){
+        try {
+            Cipher cipher = getCipher(Cipher.ENCRYPT_MODE,key);
+            byte[] bytesContent = content.getBytes("UTF-8");
+            byte[] cryptograph = cipher.doFinal(bytesContent);
+            return org.bouncycastle.util.encoders.Base64.encode(cryptograph);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    /**
+     * 解密
+     *
+     * @param cryptograph
+     *          需要解密的byte[]数据
+     * @param key
+     *          key
+     * @return
+     *          解密之后的字符串
+     */
+    private String decrypt(byte[] cryptograph,String key){
+        try {
+            Cipher cipher = getCipher(Cipher.DECRYPT_MODE,key);
+            byte[] result = cipher.doFinal(org.bouncycastle.util.encoders.Base64.decode(cryptograph));
+            return new String(result);
+        }catch (Exception e){
             e.printStackTrace();
         }
         return null;
     }
 
-    private static String decrypt(byte[] cryptograph, String key) {
+    private byte[] tohash256Deal(String key){
         try {
-            Security.addProvider(new BouncyCastleProvider());
-            //"AES"：请求的密钥算法的标准名称
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            SecureRandom securerandom = new SecureRandom(tohash256Deal(key));
-
-            kgen.init(256, securerandom);
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
-
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-            byte[] content = cipher.doFinal(Base64.decode(cryptograph));
-            return new String(content);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private static String parseByte2HexStr(byte buf[]) {
-        StringBuffer stringBuffer = new StringBuffer();
-        for (int i = 0; i < buf.length; i++) {
-            String hex = Integer.toHexString(buf[i] & 0xFF);
-            if (hex.length() == 1) {
-                hex = '0' + hex;
-            }
-            stringBuffer.append(hex.toUpperCase());
-        }
-        return stringBuffer.toString();
-    }
-
-    private static byte[] tohash256Deal(String datastr) {
-        try {
-            MessageDigest digester=MessageDigest.getInstance("SHA-256");
-            digester.update(datastr.getBytes());
-            byte[] hex=digester.digest();
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(key.getBytes());
+            byte[] hex = digest.digest();
             return hex;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e.getMessage());
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
         }
+        return null;
+    }
+
+    /**
+     * 获取*.keyStore文件中的密钥
+     *
+     * @return
+     *      获取到的密钥
+     */
+    private String getPrivateKey(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream("url");
+            //*.keyStore文件加密方式是JCEKS
+            KeyStore keyStore = KeyStore.getInstance("JCEKS");
+            //密钥库和别名密码相同
+            String password = "44053662";
+            String alias = "effort";
+            keyStore.load(fileInputStream,password.toCharArray());
+            fileInputStream.close();
+            return encodeBase64(keyStore.getKey(alias,password.toCharArray()).toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //字符串加密
+    private String encodeBase64(String str) throws UnsupportedEncodingException {
+        String bash64Str = new BASE64Encoder().encode(str.getBytes("UTF-8"));
+        return bash64Str;
+    }
+    //字符串解密
+    private String decodeBase64(String base64Str) throws IOException {
+        byte[] bytes = new BASE64Decoder().decodeBuffer(base64Str);
+        String result = new String(bytes,"UTF-8");
+        return result;
+    }
+
+    /**
+     * 转化为16进制数
+     *
+     * @param bytes
+     * @return
+     *          16进制数
+     */
+    private String getHexString(byte[] bytes){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i=0;i<bytes.length;i++){
+            String string = Integer.toHexString(bytes[i] & 0XFF).toUpperCase();
+            if (string.length() == 2)
+                stringBuilder.append(string);
+            else
+                stringBuilder.append("0").append(string);
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 将16进制的字符串转化为byte数组
+     * @param hexStr
+     * @return
+     */
+    private byte[] hexStringToByte(String hexStr){
+        if (null == hexStr || "".equals(hexStr))
+            return null;
+        hexStr = hexStr.toUpperCase();
+        int length = hexStr.length() >> 1;
+        char[] hexChar = hexStr.toCharArray();
+        byte[] bytes = new byte[length];
+        for (int i=0;i<length;i++){
+            int pos = i << 1;
+            bytes[i] = (byte) (charToByte(hexChar[pos]) << 4 | charToByte(hexChar[pos + 1]));
+        }
+        return bytes;
+    }
+
+    private byte charToByte(char c){
+        return (byte) "0123456789ABCDEF".indexOf(c);
+    }
+
+    /**
+     * 提供公开的生成加密文件的方法
+     *
+     * @param url
+     *          需要生成文件的位置
+     * @param content
+     *          需要加密的数据
+     * @throws IOException
+     */
+    public void createKeyFile(String url,String content) throws IOException {
+        try {
+            String key = getPrivateKey();
+            String encrypyKey = getHexString(encrypt(content,key));
+            Writer writer = new FileWriter(url);
+            writer.write(encrypyKey);
+            writer.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 读取加密文件中的信息
+     *
+     * @param url
+     *          读取信息文件的位置
+     * @return
+     *          解析文件之后的内容
+     */
+    public String readKeyFile(String url){
+        try {
+            String key = getPrivateKey();
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(url));
+            String temp = "";
+            while ((temp = bufferedReader.readLine())!=null){
+                stringBuilder.append(temp);
+            }
+            temp = stringBuilder.toString();
+            byte[] result = hexStringToByte(temp);
+            return decrypt(result,key);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static void main(String[] args) {
@@ -92,10 +224,10 @@ public class AES256ForBC {
         String key = "zxyy";
         String password = "0f607264fc6318a92b9e13c65db7cd3c";
 
-        byte[] encryptResult = AES256ForBC.encrypt(password, key);
-        System.out.println("密文：" + AES256ForBC.parseByte2HexStr(encryptResult));
+        byte[] encryptResult = new AES256ForBC().encrypt(password, key);
+        System.out.println("密文：" + new AES256ForBC().getHexString(encryptResult));
 
-        String decryptResult = AES256ForBC.decrypt(encryptResult, key);
+        String decryptResult = new AES256ForBC().decrypt(encryptResult, key);
         System.out.println("解密：" + decryptResult);
     }
 }
---
+++ src/com/effort/encryption/TestDEmo.java
@@ -0,0 +1,12 @@
+package com.effort.encryption;
+
+public class TestDEmo {
+    public static void main(String[] args) {
+
+        int i = 4;
+
+        System.out.println(i >> 1);
+        System.out.println(i << 1);
+
+    }
+}
